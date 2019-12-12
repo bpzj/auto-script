2f3927ff863758b9efd11fcd3533cd8c56799c0d
bpzj <1521189886@qq.com>
2019-12-12 19:33:52 +0800

    部署脚本

---
+++ linux/inotify.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+
+src=/root
+#/usr/local/bin/inotifywait -mrq --timefmt '%d/%m/%y %H:%M' --format '%T %w%f%e' -e modify,delete,create,attrib $src |  while read file
+#do
+#      /usr/bin/rsync -arzuq --delete --progress $src 192.168.136.128::www/
+#      echo "  ${file} was rsynced" >>/tmp/rsync.log 2>&1
+#done
+
+inotifywait -mrq --timefmt '%d/%m/%y %H:%M' --format '%T %w%f%e' -e modify,create $src |  while read file
+do
+      echo "  ${file} was rsynced" >> /root/rsync.log 2>&1
+done
