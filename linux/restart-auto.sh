d2a06b69cd31395ad7140be76b1638117de9b205
bpzj <1521189886@qq.com>
2020-07-08 23:35:58 +0800

    脚本换行

--- linux/restart-auto.sh
+++ linux/restart-auto.sh
@@ -1,13 +1,25 @@
 #!/usr/bin/env bash
 # 通过检查本地文件是否变动, 来重启进程
 # TODO 增加项目参数, 适配不同的项目
-jarFile="/home/nginx/webapps/ypgl-1.0.0.jar"
-jarFile="/root/1.txt"
+#  $# 为参数个数, $1 为第一个参数
+if [ $# -eq 0 ] ; then
+  echo "没有参数, 请输入要重启的 jar 文件"
+  exit 0
+fi
+if [ ! -f $1 ] ; then
+  echo "文件不存在"
+  exit 0
+fi
+if [[ ! ${1##*.} == "jar" ]] ; then
+  echo "文件不是jar文件"
+  exit 0
+fi
 
+log=`echo $1 | sed 's/.jar$/.log/g'`
 # 上一次文件状态
-last=`stat $jarFile |grep Modify`
+last=`stat $1 |grep Modify`
 while [[ "" == "" ]]; do
-  update=`stat $jarFile |grep Modify`
+  update=`stat $1 |grep Modify`
   if [[ "$update" == "$last" ]]; then
     echo "file is old"
     sleep 1
@@ -17,11 +29,11 @@ while [[ "" == "" ]]; do
     while [[ "" == "" ]]; do
       if [[ "$update" == "$last" ]]; then
         echo "update file finish"
-        process=`ps -ef|grep "java -jar"|grep ypgl-1.0.0.jar |grep -v grep|awk '{print $2}'`
+        process=`ps -ef|grep "java -jar"|grep $1 |grep -v grep|awk '{print $2}'`
         if [ -n "$process" ] ; then
           kill -9 $process
         fi
-        cd /home/nginx/webapps && nohup java -jar ypgl-1.0.0.jar --spring.profiles.active=int > ypgl.log &
+        cd /home/nginx/webapps && nohup java -jar $1 $2 > $log 2>&1 &
         # 把更新后的赋值给 last
         last=$update
 
@@ -30,7 +42,7 @@ while [[ "" == "" ]]; do
       else
         last=$update
         sleep 1
-        update=`stat $jarFile |grep Modify`
+        update=`stat $1 |grep Modify`
       fi
     done
   fi
