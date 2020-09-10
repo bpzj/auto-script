9f66ec91474253e1278522e2d518f79caca14798
bpzj <1521189886@qq.com>
2020-09-10 22:32:02 +0800

    update

--- python/股票/保存股票交割单.py
+++ python/股票/保存股票交割单.py
@@ -13,11 +13,39 @@ stock_list: List[str] = ['兴业银行']
 start_row = 5
 
 
-def get_text() -> str:
+def get_clipboard() -> str:
     w.OpenClipboard()
     d = w.GetClipboardData(win32con.CF_TEXT)
     w.CloseClipboard()
     return d.decode('GBK')
+
+
+def parse_to_list(clip: str) -> List[List[str]]:
+    if "成交日期" not in clip:
+        # todo 抛出异常
+        return None;
+    lines = clip.split('\r\n')
+    result = []
+    for i in range(1, len(lines) + 1):
+        result.append(lines[i].split('\t')[0:9])
+    return result
+
+
+def save_by_stock_name_and_date(lines: List[List[str]], name: str, day: str):
+    try:
+        xlsx = xlwings.Book(excel_path + name + '交割单.xlsx')
+        sheet = Sheet(xlsx.sheets[0])
+    except FileNotFoundError as e:
+        print(e)
+        return
+
+    for line in lines:
+        if line[0] == day and line[2] == name:
+            sheet.api.Rows(start_row).Insert()
+            for i in range(1, len(line) + 1):
+                sheet.range(start_row, i).value = line[i - 1]
+
+            # name = line[2] + '交割单.xlsx'
 
 
 # todo 把 main 方法提出为函数
@@ -25,20 +53,7 @@ def get_text() -> str:
 
 if __name__ == '__main__':
     today = datetime.datetime.today().date().strftime('%Y%m%d')
-    c = get_text()
-    li = c.split('\r\n')
-    for name in stock_list:
-        try:
-            xlsx = xlwings.Book(excel_path + name + '交割单.xlsx')
-            sheet = Sheet(xlsx.sheets[0])
-        except:
-            continue
+    data = get_clipboard()
+    li = parse_to_list(data)
+    save_by_stock_name_and_date(li, '兴业银行', today)
 
-        for i in li:
-            line = i.split('\t')[0:9]
-            if line[0] == today and line[2] == name:
-                sheet.api.Rows(start_row).Insert()
-                for i in range(1, len(line) + 1):
-                    sheet.range(start_row, i).value = line[i - 1]
-
-                # name = line[2] + '交割单.xlsx'
