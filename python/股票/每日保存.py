c2b8e6f7ea4bdf20d96032cc0534afd52c92e3fa
bpzj <1521189886@qq.com>
2020-11-12 18:00:46 +0800

    update

--- python/股票/每日保存.py
+++ python/股票/每日保存.py
@@ -10,8 +10,52 @@ create_table_sql = """create table check_list (id integer constraint check_list_
                         date text, code text, name text, b_s text, quantity int,
                         price decimal(10,4), actual_amount decimal(15,4), available_balance int, contract int); """
 
+
 # todo 根据表头得到每一列具体是什么, 把每一列的下标做成变量， 不写死
-title = ['成交日期', '成交时间', '证券代码', '证券名称', '操作', '成交数量', '成交均价', '成交金额', '发生金额', '可用余额', '合同编号']
+
+class DataIndex:
+    def __init__(self):
+        self.date_idx = -1
+        self.time_idx = -1
+        self.code_idx = -1
+        self.name_idx = -1
+        self.bs_idx = -1
+        self.quantity_idx = -1
+        self.price_idx = -1
+        self.actual_amount_idx = -1
+        self.amount_idx = -1
+        self.available_balance_idx = -1
+        self.contract_idx = -1
+
+
+def get_index(li: List[str]) -> DataIndex:
+    idx = DataIndex()
+    for i in range(0, len(li)):
+        line = li[i]
+        if line == '成交日期':
+            idx.date_idx = i
+        elif line == '成交时间':
+            idx.time_idx = i
+        elif line == '证券代码':
+            idx.code_idx = i
+        elif line == '证券名称':
+            idx.name_idx = i
+        elif line == '操作':
+            idx.bs_idx = i
+        elif line == '成交数量':
+            idx.quantity_idx = i
+        elif line == '成交均价':
+            idx.price_idx = i
+        elif line == '成交金额':
+            idx.amount_idx = i
+        elif line == '发生金额':
+            idx.actual_amount_idx = i
+        elif line == '可用余额':
+            idx.available_balance_idx = i
+        elif line == '合同编号':
+            idx.contract_idx = i
+
+    return idx
 
 
 def get_clipboard() -> str:
@@ -33,7 +77,7 @@ def parse_to_list(clip: str) -> List[List[str]]:
     lines = clip.split('\r\n')
     get_date_type(lines[0])
     result = []
-    for i in range(1, len(lines)):
+    for i in range(0, len(lines)):
         line = lines[i].split('\t')[0:14]
         if line[2] is not 'Ｒ-001':
             result.append(line)
@@ -41,49 +85,55 @@ def parse_to_list(clip: str) -> List[List[str]]:
 
 
 def save_today_stock_check_list(lines: List[List[str]]):
+    if not lines:
+        return
     db_file = r'E:\\OneDrive\\文档\\2、生活口才\\股票交割单\\所有交割单.db'
     conn = sqlite3.connect(db_file)
     cursor = conn.cursor()
-    for line in lines:
-        value = get_insert_sql(line)
+    data_idx = get_index(lines[0])
+    for line in lines[1:]:
+        sql = get_insert_sql(data_idx, line)
         # todo 根据日期和合同号判断数据库中是否已经保存过了
-        # print(value)
-        cursor.execute('select * from check_list where date=\'' + line[0] + '\' and contract=\'' + line[13] + '\'')
+        cursor.execute('select * from check_list where date=\'' + line[data_idx.date_idx] + '\' and contract=\'' + line[
+            data_idx.contract_idx] + '\'')
         v = cursor.fetchall()
-        if not v:
-            cursor.execute(
-                'insert into check_list (date, code, name, b_s, quantity, price, actual_amount, available_balance, contract) values' + value)
+        if not v and len(v) == 0:
+            print(v)
+            # cursor.execute(sql)
 
     # 通过rowcount获得插入的行数:
     # print('rowcount =', cursor.rowcount)
-    # 关闭Cursor:
     cursor.close()
-    # 提交事务:
     conn.commit()
-    # 关闭Connection:
     conn.close()
-    #
-    # for line in lines:
-    #     if line[0] <= day and line[2] == name:
-    #         sheet.api.Rows(start_row).Insert()
-    #         for i in range(1, len(line) + 1):
-    #             sheet.range(start_row, i).value = line[i - 1]
-    #
-    #         if line[3] == '卖':
-    #             sheet.range(start_row, 10).value = '-' + line[4]
-    #         else:
-    #             sheet.range(start_row, 10).value = line[4]
-    # name = line[2] + '交割单.xlsx'
 
 
-def get_insert_sql(line):
-    s2 = ','
-    s3 = ',\''
+def get_insert_sql(data_idx: DataIndex, line: List[str]):
+    date = line[data_idx.date_idx]
+    code = line[data_idx.code_idx]
+    name = line[data_idx.name_idx]
+    b_s = line[data_idx.bs_idx]
+    quantity = line[data_idx.quantity_idx]
+    price = line[data_idx.price_idx]
+    if data_idx.actual_amount_idx:
+        actual_amount = line[data_idx.actual_amount_idx]
+    else:
+        # todo 真实金额
+        actual_amount = quantity * price
+    if data_idx.available_balance_idx >= 0:
+        available_balance = line[data_idx.available_balance_idx]
+    time = line[data_idx.time_idx] if data_idx.time_idx >= 0 else None
+    contract = line[data_idx.contract_idx] if data_idx.contract_idx >= 0 else ''
     s1 = '\',\''
-    # print(line[2])
-    contract_no = s3 if line[13] == '' else (s3 + line[13])
-    value = '(\'' + s1.join(line[0:4]) + '\',' + s2.join(line[4:6]) + s2 + line[8] + s2 + line[7] + contract_no + '\')'
-    return value
+    s2 = '\','
+    s3 = ','
+    sql_value = '(\'' + date
+    sql_value = (sql_value + s1 + time) if time else (sql_value + s1)
+    sql_value = sql_value + s1 + code + s1 + name + s1 + b_s + s2
+    sql_value = sql_value + quantity + s3 + price + s3 + actual_amount + s3 + available_balance
+    sql_value = (sql_value + ',\'' + contract + '\')') if contract else (sql_value + ',\'\')')
+    sql = "insert into check_list (date, time, code, name, b_s, quantity, price, actual_amount, available_balance, contract) values" + sql_value
+    return sql
 
 
 def create_table():
@@ -103,6 +153,8 @@ def create_table():
 
 
 # todo 把 main 方法提出为函数
+
+
 if __name__ == '__main__':
     # create_table()
     save_today_stock_check_list(parse_to_list(get_clipboard()))
