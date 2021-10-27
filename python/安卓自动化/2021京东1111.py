e2d07983e481ed0aeae651531239cf8f8546d32c
liqt <liqtr@yonyou.com>
2021-10-27 17:14:58 +0800

    update

--- python/安卓自动化/2021京东1111.py
+++ python/安卓自动化/2021京东1111.py
@@ -1,9 +1,26 @@
 import time
 
-def click_sleep_back(d,x,y,t):
-    d.click(x,y)
+share_page_line_2 = (0.790, 0.572)
+share_page_line_4 = (0.791, 0.756)
+share_page_line_5 = (0.791, 0.846)
+
+
+def click_then_back(pos: tuple, times: int):
+    for i in range(times):
+        d.click(*pos)
+        time.sleep(20)
+        d.press('back')
+        time.sleep(5)
+
+
+click_then_back(share_page_line_5, 10)
+
+
+def click_sleep_back(d, x, y, t):
+    d.click(x, y)
     time.sleep(t)
     d.press("back")
+
 
 for i in range(5):
     time.sleep(10)
@@ -17,15 +34,6 @@ for i in range(5):
     click_sleep_back(d, 0.293, 0.885, 2)
 
     d.press("back")
-
-
-
-for i in range(25):
-    d.click(0.791, 0.756)
-    time.sleep(20)
-    d.press('back')
-    time.sleep(5)
-
 
 for i in range(25):
     d.click(0.791, 0.572)
--- python/安卓自动化/2021淘宝1111.py
+++ python/安卓自动化/2021淘宝1111.py
@@ -1,10 +1,18 @@
-import time
 
 # pip install weditor  安装
 # python -m weditor 开启浏览器，开启手机调试
 
-for i in range(25):
-    d.click(0.852, 0.687)
-    time.sleep(30)
-    d.press('back')
-    time.sleep(8)
+import time
+def click_then_back(pos: tuple, times: int):
+    for i in range(times):
+        d.click(*pos)
+        time.sleep(30)
+        d.press('back')
+        time.sleep(8)
+
+
+share_page_line_3 = (0.852, 0.687)
+share_page_line_4 = (0.791, 0.756)
+share_page_line_5 = (0.791, 0.846)
+
+click_then_back(share_page_line_3,30)
