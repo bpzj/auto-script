bb1079900323e080316ed291ca807e08af71a964
liqt <liqtr@yonyou.com>
2021-10-28 20:26:33 +0800

    update

--- python/安卓自动化/2021淘宝1111.py
+++ python/安卓自动化/2021淘宝1111.py
@@ -8,7 +8,7 @@ import time
 def click_auto_once(t):
     if d(text=t).exists:
         d(text=t).click()
-        time.sleep(5)
+        time.sleep(10)
         if d(text='亲，访问被拒绝').exists:
             d.press("back")
             time.sleep(0.05)
