b4afedef0ccf9c6e1a7a273f4c7e8c0801aa9737
liqt <liqtr@yonyou.com>
2021-12-09 09:51:53 +0800

    update

--- python/安卓自动化/2021京东1111.py
+++ python/安卓自动化/2021京东1111.py
@@ -1,42 +1,12 @@
 import time
 
-share_page_line_2 = (0.790, 0.572)
-share_page_line_4 = (0.791, 0.756)
-share_page_line_5 = (0.791, 0.846)
 
-
-def click_then_back(pos: tuple, times: int):
+def click_sleep_back(x, y, before_click: int, after_click: int, times: int):
     for i in range(times):
-        d.click(*pos)
-        time.sleep(20)
-        d.press('back')
-        time.sleep(5)
+        time.sleep(before_click)
+        d.click(x, y)
+        time.sleep(after_click)
+        d.press("back")
 
 
-click_then_back(share_page_line_5, 10)
-
-
-def click_sleep_back(d, x, y, t):
-    d.click(x, y)
-    time.sleep(t)
-    d.press("back")
-
-
-for i in range(5):
-    time.sleep(10)
-    d.click(0.835, 0.754)
-    time.sleep(10)
-
-    click_sleep_back(d, 0.160, 0.294, 2)
-    click_sleep_back(d, 0.786, 0.329, 2)
-    click_sleep_back(d, 0.288, 0.642, 2)
-    click_sleep_back(d, 0.808, 0.685, 2)
-    click_sleep_back(d, 0.293, 0.885, 2)
-
-    d.press("back")
-
-for i in range(25):
-    d.click(0.791, 0.572)
-    time.sleep(20)
-    d.press('back')
-    time.sleep(5)
+click_sleep_back(0.825, 0.795, 10, 10, 5)
